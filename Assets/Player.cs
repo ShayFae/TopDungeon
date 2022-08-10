a34b4542e898d014ab35d5a6e0b0c5175d16d080
Ifrah Noor <78050266+ShayFae@users.noreply.github.com>
2022-08-10 17:26:56 -0400

    Add player movement and collision detection

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+ private BoxCollider2D boxCollider;
+ private Vector3 moveDelta;
+ private RaycastHit2D hit;
+
+ private void Start() {
+    boxCollider = GetComponent<BoxCollider2D>();
+ }
+
+ private void FixedUpdate() {
+
+    float x = Input.GetAxisRaw("Horizontal");
+    float y = Input.GetAxisRaw("Vertical");
+
+    //Reset moveDelta
+    moveDelta = new Vector3(x, y, 0);
+
+    //Switch sprite direction for left or right
+    if (moveDelta.x > 0) 
+        transform.localScale = Vector3.one;
+    else if (moveDelta.x < 0)
+        transform.localScale = new Vector3(-1, 1, 1);
+
+    hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Human", "Blocking"));
+    if (hit.collider == null) {
+        //Make the sprite move
+        transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Human", "Blocking"));
+    if (hit.collider == null) {
+        //Make the sprite move
+        transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
     }
+ 
+ }
 }
